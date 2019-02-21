b865d68de1d57f46375665d37242516ab1c676e9
Alex Kondratenko <avk25990@ucmo.edu>
2019-02-21 14:09:39 -0600

    Updated react application
    Edited the react application to send hard coded requests to Plino API

--- plino/web/src/App.js
+++ plino/web/src/App.js
@@ -3,26 +3,73 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      emailtext: "none"
+    };
   }
+
+  componentDidMount() {
+    fetch("https://cors-anywhere.herokuapp.com/https://plino.herokuapp.com/api/v1/classify/", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email_text: 'my uncle ben!'
+      })
+    })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.email_class
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+
+  render() {
+    const {error, isLoaded, items} = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              {this.emailtext}
+            </p>
+
+            <form onSubmit={this.handleSubmit}>
+              <label>Message:
+                <input type="submit" value="Submit" />
+              </label>
+              
+            </form>
+            {items}
+          </header>
+        </div>
+      );
+    }
+  }
+
 }
 
 export default App;
