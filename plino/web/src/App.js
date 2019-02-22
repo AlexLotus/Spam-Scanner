5d3613a7bcc775e77c3b31b1c59341e883206286
Shane-Lewes Tenorio <sltenorio99@gmail.com>
2019-02-21 20:20:22 -0600

    💅 Update submission UI, add submit function, allow message changing

--- plino/web/src/App.js
+++ plino/web/src/App.js
@@ -9,11 +9,28 @@ class App extends Component {
       error: null,
       isLoaded: false,
       items: [],
-      emailtext: "none"
+      emailtext: "none",
+      message: "my uncle ben!"
     };
   }
 
   componentDidMount() {
+    this.checkMessage();
+  }
+
+  onMessageChange = e => {
+    this.setState({ message: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    this.checkMessage();
+  };
+
+  checkMessage = () => {
+    const { message } = this.state;
     fetch(
       "https://cors-anywhere.herokuapp.com/https://plino.herokuapp.com/api/v1/classify/",
       {
@@ -23,7 +40,7 @@ class App extends Component {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          email_text: "my uncle ben!"
+          email_text: message
         })
       }
     )
@@ -42,73 +59,73 @@ class App extends Component {
           });
         }
       );
-  }
+  };
 
   render() {
     const { error, isLoaded, items } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <div className="App">
-          <nav
-            class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top App-header"
-            id="mainNav"
-          >
-            <div class="container">
-              <a class="navbar-brand" href="#page-top">
-                <img src={logo} className="App-logo" alt="logo" />
-                Spam Filter
-              </a>
-              <button
-                class="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarResponsive"
-                aria-controls="navbarResponsive"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span class="navbar-toggler-icon" />
-              </button>
-              <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                  <li class="nav-item">
-                    <a class="nav-link" href="#about">
-                      About
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#services">
-                      Filtering
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#contact">
-                      Contact
-                    </a>
-                  </li>
-                </ul>
-              </div>
+    return (
+      <div className="App">
+        <nav
+          className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top App-header"
+          id="mainNav"
+        >
+          <div className="container">
+            <a className="navbar-brand" href="#page-top">
+              <img src={logo} className="App-logo" alt="logo" />
+              Spam Filter
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarResponsive"
+              aria-controls="navbarResponsive"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <a className="nav-link" href="#about">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#services">
+                    Filtering
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#contact">
+                    Contact
+                  </a>
+                </li>
+              </ul>
             </div>
-          </nav>
+          </div>
+        </nav>
 
-          <header class="bg-primary text-white">
-            <div class="container text-center">
-              <h1>Spammy Spam Filtering</h1>
-              <p class="lead">A spam filtering application for your inbox!</p>
-            </div>
-          </header>
+        <header className="bg-primary text-white">
+          <div className="container text-center">
+            <h1>Spammy Spam Filtering</h1>
+            <p className="lead">A spam filtering application for your inbox!</p>
+          </div>
+        </header>
 
-          <section id="about">
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-8 mx-auto">
-                  <h2>Test out our spam service!</h2>
-                  <p class="lead">Insert some text for testing</p>
-                  <p>{this.emailtext}</p>
+        <section id="about">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8 mx-auto">
+                <h2>Test out our spam service!</h2>
+                <p className="lead">Insert some text for testing</p>
+                <p>{this.emailtext}</p>
+                {!isLoaded ? (
+                  <div>Loading...</div>
+                ) : error ? (
+                  <div>Error: {error.message}</div>
+                ) : (
                   <div className="card">
                     <div className="card-body">
                       <form onSubmit={this.handleSubmit} className="text-left">
@@ -118,6 +135,7 @@ class App extends Component {
                             type="text"
                             placeholder="Insert message here"
                             className="form-control"
+                            onChange={this.onMessageChange}
                           />
                         </div>
                         <input
@@ -131,53 +149,53 @@ class App extends Component {
                       <strong>Result: {items}</strong>
                     </div>
                   </div>
-                </div>
+                )}
               </div>
             </div>
-          </section>
+          </div>
+        </section>
 
-          <section id="services" class="bg-light">
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-8 mx-auto">
-                  <h2>Services we offer</h2>
-                  <p class="lead">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Aut optio velit inventore, expedita quo laboriosam possimus
-                    ea consequatur vitae, doloribus consequuntur ex. Nemo
-                    assumenda laborum vel, labore ut velit dignissimos.
-                  </p>
-                </div>
+        <section id="services" className="bg-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8 mx-auto">
+                <h2>Services we offer</h2>
+                <p className="lead">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
+                  optio velit inventore, expedita quo laboriosam possimus ea
+                  consequatur vitae, doloribus consequuntur ex. Nemo assumenda
+                  laborum vel, labore ut velit dignissimos.
+                </p>
               </div>
             </div>
-          </section>
+          </div>
+        </section>
 
-          <section id="contact">
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-8 mx-auto">
-                  <h2>Contact us</h2>
-                  <p class="lead">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Vero odio fugiat voluptatem dolor, provident officiis, id
-                    iusto! Obcaecati incidunt, qui nihil beatae magnam et
-                    repudiandae ipsa exercitationem, in, quo totam.
-                  </p>
-                </div>
+        <section id="contact">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8 mx-auto">
+                <h2>Contact us</h2>
+                <p className="lead">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero
+                  odio fugiat voluptatem dolor, provident officiis, id iusto!
+                  Obcaecati incidunt, qui nihil beatae magnam et repudiandae
+                  ipsa exercitationem, in, quo totam.
+                </p>
               </div>
             </div>
-          </section>
+          </div>
+        </section>
 
-          <footer class="py-5 bg-dark">
-            <div class="container">
-              <p class="m-0 text-center text-white">
-                Copyright &copy; Spammy 2019
-              </p>
-            </div>
-          </footer>
-        </div>
-      );
-    }
+        <footer className="py-5 bg-dark">
+          <div className="container">
+            <p className="m-0 text-center text-white">
+              Copyright &copy; Spammy 2019
+            </p>
+          </div>
+        </footer>
+      </div>
+    );
   }
 }
 
