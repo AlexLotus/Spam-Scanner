ad9d63c6c86dc7f24d349ba975573b0a4e169cba
Alex Kondratenko <avk25990@ucmo.edu>
2019-03-15 13:45:23 -0500

    Updates

---
+++ precache-manifest.9d17382a88e751d7c89bac68babc2146.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c49b5edd94c5a254f7ea",
+    "url": "/static/css/main.aa9ce3b1.chunk.css"
+  },
+  {
+    "revision": "c49b5edd94c5a254f7ea",
+    "url": "/static/js/main.8f548a55.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c481632a0a122047042c",
+    "url": "/static/js/2.f7b938d0.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "734523df7df7fdb023d4782bed71aa75",
+    "url": "/static/media/ss.734523df.png"
+  },
+  {
+    "revision": "39ccc203f2f916ca3444330a345a65b3",
+    "url": "/index.html"
+  }
+];
