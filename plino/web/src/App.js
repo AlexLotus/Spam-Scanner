b38cb525ec5eb27549a2efcf97654391257f8b98
Shane-Lewes Tenorio <sltenorio99@gmail.com>
2019-04-13 00:52:11 -0500

    🎨 Fix unscrollable page

--- plino/web/src/App.js
+++ plino/web/src/App.js
@@ -9,60 +9,65 @@ import NotFound from "./components/NotFound";
 import Secret from "./components/Secret";
 import Callback from "./components/Callback";
 import Main from "./components/Main";
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
 class App extends Component {
- 
-
   render() {
     let mainComponent = "";
-        switch(this.props.location){
-            case "":
-                mainComponent = <Main {...this.props} />;
-                break;
-            case "home":
-                mainComponent = <Home />;
-                break;
-            case "callback":
-                mainComponent = <Callback />;
-                break;
-            case "new":
-                mainComponent = < UpdatedFilter />;
-                break;
-            case "secret":
-                mainComponent = this.props.auth.isAuthenticated() ? <Secret {...this.props} /> : <NotFound/>;
-                break;
-            case "filter":
-                mainComponent = <Filter />;
-            default:
-                mainComponent = <NotFound />;
-        }
+    switch (this.props.location) {
+      case "":
+        mainComponent = <Main {...this.props} />;
+        break;
+      case "home":
+        mainComponent = <Home />;
+        break;
+      case "callback":
+        mainComponent = <Callback />;
+        break;
+      case "new":
+        mainComponent = <UpdatedFilter />;
+        break;
+      case "secret":
+        mainComponent = this.props.auth.isAuthenticated() ? (
+          <Secret {...this.props} />
+        ) : (
+          <NotFound />
+        );
+        break;
+      case "filter":
+        mainComponent = <Filter />;
+      default:
+        mainComponent = <NotFound />;
+    }
     return (
-      <nav
-      className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top App-header"
-      id="mainNav"
-    >
       <div className="App">
-      <header className="App-Header">
-      <img src={logo} className="App-logo" alt="logo" />
-      {/* So this is where the header gets cut off from the logo (if you uncomment the router)...need to fix 
+        <nav
+          className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top App-header"
+          id="mainNav"
+        >
+          <header className="App-Header">
+            <img src={logo} className="App-logo" alt="logo" />
+            {/* So this is where the header gets cut off from the logo (if you uncomment the router)...need to fix 
           Also possible remove the router and replace with a full switch case
       */}
-      <h3 className="App-Title">Welcome to our React App, {this.props.name}</h3>
-      </header>
-      {mainComponent}
-      <p className="App-Intro">
-      It's the greatest thing you never knew you needed</p>
-<p className="dark"> </p>
+            <h3 className="App-Title">
+              Welcome to our React App, {this.props.name}
+            </h3>
+          </header>
+        </nav>
+        {mainComponent}
+        <p className="App-Intro">
+          It's the greatest thing you never knew you needed
+        </p>
+        <p className="dark"> </p>
         <footer className="py-5 bg-dark">
           <div className="container">
             <p className="m-0 text-center text-white">
-              Copyright &copy; Spammy 2019 
+              Copyright &copy; Spammy 2019
             </p>
           </div>
         </footer>
       </div>
-      </nav>
     );
   }
 }
