965b567e532a79f08566179a1d50ba745be35e2a
Shane-Lewes Tenorio <sltenorio99@gmail.com>
2019-04-13 07:38:59 -0500

    😂 Update footer

--- plino/web/src/App.js
+++ plino/web/src/App.js
@@ -94,7 +94,7 @@ class App extends Component {
           <footer className="py-3 bg-dark">
             <div className="container col">
               <p className="m-0 text-center text-white">
-                Copyright &copy; Spammy 2019
+                Copyright &copy; Spammy-Scanner 2019
               </p>
             </div>
           </footer>
